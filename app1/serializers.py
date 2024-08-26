078c57bdf543f3bc4dad9304227705f6034f155f
OliverDLSR <60279058+OliverDLSR@users.noreply.github.com>
2024-08-26 09:30:44 -0400

    Update serializers.py

--- app1/serializers.py
+++ app1/serializers.py
@@ -5,3 +5,15 @@ class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = '__all__'
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ['codigo', 'descripcion', 'precio', 'tipo']
+    
+    def validate_precio(self, value):
+        if not isinstance(value, (int, float)):  # Asegúrate de que el precio sea un número
+            raise serializers.ValidationError("El precio debe ser un número.")
+        if value < 100:
+            raise serializers.ValidationError("El precio debe ser al menos 100.")
+        return value
